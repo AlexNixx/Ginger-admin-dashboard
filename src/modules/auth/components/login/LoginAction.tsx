1bb66fdfd30a634f1644b52bf7bf2b5d4e217839
AlexNix <alexnix.mail@gmail.com>
2023-07-01 22:06:18 +0300

    update login action component

--- src/modules/auth/components/login/LoginAction.tsx
+++ src/modules/auth/components/login/LoginAction.tsx
@@ -1,23 +1,54 @@
-import { Row, Col } from "antd";
+import { Col, Row, Spin, Alert, Space, Image } from "antd";
 import { LoginForm, LoginFormValues } from "./LoginForm";
-import { login } from "modules/auth/services/authServices";
 
-import { AxiosError } from "axios";
+import logo from "shared/assets/image/logo.svg";
+
+import { useAuthStore } from "modules/auth/models/useAuth";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-hot-toast";
+import { useEffect } from "react";
 
 export const LoginAction = () => {
+	const { login, isSuccess, isLoading, isError, error } = useAuthStore();
+	const navigate = useNavigate();
+
 	const onFinish = async (loginData: LoginFormValues) => {
-		try {
-			const { email, password } = loginData;
-			const response = await login(email, password);
-		} catch (error) {
-			const err = error as AxiosError;
-			console.log(err.response?.data);
-		}
+		const { email, password } = loginData;
+
+		await login(email, password);
 	};
+
+	useEffect(() => {
+		if (isError) {
+			toast.error(error);
+		}
+	}, [isError]);
+
+	useEffect(() => {
+		if (isSuccess) {
+			navigate("/");
+		}
+	}, [isSuccess]);
+
+	if (isLoading) {
+		return (
+			<Row justify={"center"} align={"middle"} style={{ height: "100vh" }}>
+				<Spin size="large" />
+			</Row>
+		);
+	}
 
 	return (
 		<Row justify={"center"} align={"middle"} style={{ height: "100vh" }}>
 			<Col span={6}>
+				<Image
+					src={logo}
+					alt="logo"
+					height={"3rem"}
+					width={"100%"}
+					preview={false}
+					style={{ marginBottom: "8rem" }}
+				/>
 				<LoginForm onFinish={onFinish} />
 			</Col>
 		</Row>
