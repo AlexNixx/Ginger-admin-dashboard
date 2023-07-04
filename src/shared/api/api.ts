4b85f6fb0c6ba7d3723cd70867fd9ab4f852f81f
AlexNix <alexnix.mail@gmail.com>
2023-07-05 01:32:08 +0300

    update axios interceptors for response

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
-import { USER_LOCALSTORAGE_KEY } from "shared/const/localstorage";
-import { refreshAccessToken } from "modules/auth/services/authServices";
+
+import { useAuthStore } from "modules/auth/models/useAuth";
 
 const API_URL = `${process.env.REACT_APP_SERVER_ENDPOINT}/api`
 
@@ -9,8 +9,10 @@ export const $api = axios.create({
     withCredentials: true,
 })
 
-$api.interceptors.request.use(config => {
-    config.headers.Authorization = `Bearer ${localStorage.getItem(USER_LOCALSTORAGE_KEY)}`
+$api.interceptors.request.use(async (config) => {
+    const token = await useAuthStore.getState().accessToken;
+    config.headers.authorization = `Bearer ${token}`;
+
     return config
 },
     error => {
@@ -18,15 +20,54 @@ $api.interceptors.request.use(config => {
     }
 )
 
-$api.interceptors.response.use((response) => {
-    return response
-}, async function (error) {
-    const originalRequest = error.config;
-    if (error.response.status === 401 && !originalRequest._retry) {
-        originalRequest._retry = true;
-        const access_token = await refreshAccessToken();
-        localStorage.setItem(USER_LOCALSTORAGE_KEY, access_token.data.accessToken)
-        return $api(originalRequest);
+let isRefreshing = false;
+let failedQueue: (() => Promise<any>)[] = [];
+
+const processQueue = async () => {
+    while (failedQueue.length) {
+        try {
+            const request = failedQueue.shift();
+            if (request) {
+                await request();
+            }
+        } catch (error) {
+            console.log('Error occurred while re-sending failed request:', error);
+        }
     }
-    return Promise.reject(error);
-});
+};
+
+$api.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config;
+        if (error.response.status === 401 && !originalRequest._retry) {
+            if (isRefreshing) {
+                return new Promise((resolve, reject) => {
+                    failedQueue.push(async () => {
+                        try {
+                            const refreshedResponse = await $api(originalRequest);
+                            resolve(refreshedResponse);
+                        } catch (refreshedError) {
+                            reject(refreshedError);
+                        }
+                    });
+                });
+            }
+
+            originalRequest._retry = true;
+            isRefreshing = true;
+
+            try {
+                await useAuthStore.getState().refresh();
+                processQueue();
+                return $api(originalRequest);
+            } catch (refreshError) {
+                return Promise.reject(refreshError);
+            } finally {
+                isRefreshing = false;
+            }
+        }
+
+        return Promise.reject(error);
+    }
+);
