d58fff1d4c9c9d43cc5f04bd171d3f3c972e532f
AlexNix <alexnix.mail@gmail.com>
2023-07-05 01:31:27 +0300

    implementing logout

--- src/modules/auth/components/logout/Logout.tsx
+++ src/modules/auth/components/logout/Logout.tsx
@@ -1,3 +1,26 @@
-export const Logout = () => {
-	return <div>Logout</div>;
+import { RollbackOutlined } from "@ant-design/icons";
+import { FC } from "react";
+
+import { Typography } from "antd";
+import { useAuthStore } from "modules/auth/models/useAuth";
+
+interface LogoutProps {
+	short?: boolean;
+	className?: string;
+}
+
+export const Logout: FC<LogoutProps> = ({ short, className }) => {
+	const logout = useAuthStore((state) => state.logout);
+
+	const handleLogout = async () => {
+		const result = await logout();
+		console.log(result);
+	};
+
+	return (
+		<Typography.Link className={className} onClick={handleLogout}>
+			<RollbackOutlined style={{ color: "white" }} />
+			{!short && <p>Logout</p>}
+		</Typography.Link>
+	);
 };
